e6bfedee39b699b7ff5354e93dda89921c8898df
Nguyen Mai Thy <nguyenmaithy@Nguyens-MacBook-Pro.local>
2023-05-03 12:53:08 +0700

    LVTN-188: complete login screen

--- front-end/src/frameworks-and-drivers/vove/vove/src/screens/login/login.tsx
+++ front-end/src/frameworks-and-drivers/vove/vove/src/screens/login/login.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import {ScrollView, View, Text, Button} from "react-native";
-import {Color, ScreenSize} from "@front-end/shared/utils";
+import React, {useState} from "react";
+import {Image, ScrollView, View, Text, Pressable, Alert} from "react-native";
+import {Color, ScreenSize, TextStyle} from "@front-end/shared/utils";
 import AnimatedSplash from "react-native-animated-splash-screen";
 import {InputText} from "../../components/inputs/input-text";
 import {InputPassword} from "../../components/inputs/input-password";
+import {ButtonFullWidth} from "../../components/buttons/button-full-width";
 
 export interface LoginProps {
   navigation: any
@@ -21,31 +22,61 @@ export function Login(props: LoginProps) {
   return (
     <AnimatedSplash
       isLoaded={loading}
-      logoImage={require('../../assets/Vove.png')}
+      logoImage={require('../../images/splash.png')}
       backgroundColor={"#ffffff"}
-      logoWidth={ScreenSize.height * 0.8}
+      logoHeight={ScreenSize.width * 0.7}
+      logoWidth={ScreenSize.height * 0.7}
     >
       <ScrollView style={{backgroundColor: Color.white_100}}>
         <View style={{alignItems: "center", paddingVertical: ScreenSize.height * 0.15}}>
-          <Text>Login</Text>
-          <InputText title={"Phone number"} placeholder={"Please enter your phone number"}></InputText>
-          <InputPassword title="Password" allowOutput={false}></InputPassword>
-          <Button
-            title="Login Failed"
-            onPress={() => props.navigation.navigate("LoginFailed")}
-          />
-          <Button
-            title="Home"
-            onPress={() => props.navigation.navigate("UserStack")}
-          />
-          <Button
-            title="Sign Up"
-            onPress={() => props.navigation.navigate("SignupStack")}
-          />
-          <Button
-            title="Forget pass"
-            onPress={() => props.navigation.navigate("ForgetPasswordStack")}
-          />
+          <View style={{alignItems: "center",}}>
+            <Image
+              source={require("../../images/login.png")}
+              style={{width: ScreenSize.width * 0.67, height: ScreenSize.width * 0.6}}
+            ></Image>
+          </View>
+          <InputText
+            title={"Phone number"}
+            placeholder={"Please enter your phone number"}
+            allowOutput={true}
+            output={setUsername}
+            rightIcon={username === '' ? '' : 'check-circle-outline'}
+            keyboardType="numeric"
+          ></InputText>
+          <InputPassword
+            title="Password"
+            allowOutput={true}
+            output={setPassword}
+          ></InputPassword>
+          <View style={{
+            flexDirection: 'row',
+            marginVertical: ScreenSize.height * 0.01,
+            marginHorizontal: ScreenSize.width * 0.08,
+            width: (327 / 375) * ScreenSize.width
+          }}>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+              <Text style={{...TextStyle.h3, color: Color.grey_100}}>Remember me </Text>
+            </View>
+            <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <Pressable onPress={() => props.navigation.navigate("ForgetPasswordStack")}>
+                <Text style={{...TextStyle.h3, color: Color.primary_100}}>Forgot password?</Text>
+              </Pressable>
+            </View>
+          </View>
+
+          <ButtonFullWidth content='Log In' onPress={() => {
+            if (username === "" || password === "")
+              Alert.alert("Please enter all necessary information!");
+            else
+              props.navigation.navigate("UserStack", {username: username, password: password})
+          }}></ButtonFullWidth>
+
+          <View style={{marginTop: ScreenSize.height * 0.02, flexDirection: 'row'}}>
+            <Text style={{...TextStyle.h3, color: Color.grey_100}}>No account? </Text>
+            <Pressable onPress={() => props.navigation.navigate("SignupStack")}>
+              <Text style={{...TextStyle.h3, color: Color.primary_100}}>Crate one!</Text>
+            </Pressable>
+          </View>
         </View>
       </ScrollView>
     </AnimatedSplash>
