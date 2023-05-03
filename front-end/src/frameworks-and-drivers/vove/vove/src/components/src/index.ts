29c160e38a95c4d9981c4c36e73238798b244769
Nguyen Mai Thy <nguyenmaithy@Nguyens-MacBook-Pro.local>
2023-05-03 15:53:08 +0700

    LVTN-188: add components step bar & input date & input info & button small

--- front-end/src/frameworks-and-drivers/vove/vove/src/components/src/index.ts
+++ front-end/src/frameworks-and-drivers/vove/vove/src/components/src/index.ts
@@ -1,4 +1,7 @@
 export * from './buttons/button-full-width'
 export * from './inputs/input-password'
 export * from './inputs/input-text'
-
+export * from './inputs/input-date'
+export * from './inputs/input-information'
+export * from './buttons/button-small'
+export * from './bars/step-bar'
--- front-end/src/shared/utils/src/index.ts
+++ front-end/src/shared/utils/src/index.ts
@@ -2,3 +2,4 @@ export * from './color-style/color';
 export * from './text-style/text-style';
 export * from './screen-size/screen-size'
 export * from './button-types'
+export * from './screen-size/custom-size'
