ccb501f07da1ce52a4e4f51b05de268713bc7c02
Nguyen Mai Thy <nguyenmaithy@Nguyens-MacBook-Pro.local>
2023-05-07 16:51:33 +0700

    LVTN-254: Add back button in header

--- front-end/src/frameworks-and-drivers/vove/vove/src/navigation/user.navigator.tsx
+++ front-end/src/frameworks-and-drivers/vove/vove/src/navigation/user.navigator.tsx
@@ -1,41 +1,44 @@
 import React from 'react';
-import { Image } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ScreenSize } from '@front-end/shared/utils';
-import Home from '../screens/home/home';
+import {Image} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {ScreenSize} from '@front-end/shared/utils';
+import {UserNavBar} from "./user-nav-bar.navigator";
 
 const Stack = createStackNavigator();
 
-// const backButtonImg = () => {
-//   return (
-//     <Image
-//       source={require("../images/backButton.png")}
-//       style={{
-//         height: ScreenSize.width * 0.1,
-//         width: ScreenSize.width * 0.1,
-//         marginLeft: ScreenSize.width * 0.06,
-//       }}
-//     />
-//   );
-// };
+const backButtonImg = () => {
+  return (
+    <Image
+      source={require("../images/back-button.png")}
+      style={{
+        height: ScreenSize.width * 0.1,
+        width: ScreenSize.width * 0.1,
+        marginLeft: ScreenSize.width * 0.06,
+      }}
+    />
+  );
+};
 
-// const HeaderStyle = {
-//   headerBackImage: backButtonImg,
-//   headerStyle: {
-//     backgroundColor: "white",
-//     height: ScreenSize.height * 0.156,
-//   },
-//   headerTitleStyle: { fontSize: ScreenSize.width * 0.06 },
-//   headerTintColor: "black",
-//   headerBackTitleVisible: false,
-//   headerTitleAlign: 'center',
-//   title: "Đăng ký",
-// };
+let center = "center" as "center" | "left" | undefined;
+
+const HeaderStyle = {
+  headerBackImage: backButtonImg,
+  headerStyle: {
+    backgroundColor: "white",
+    height: ScreenSize.height * 0.1,
+  },
+  headerTitleStyle: {fontSize: ScreenSize.width * 0.05},
+  headerTintColor: "black",
+  headerBackTitleVisible: false,
+  headerTitleAlign: center,
+  title: "Home",
+};
 
 export const UserStack = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator screenOptions={HeaderStyle} initialRouteName="Home">
+      <Stack.Screen name="Home" component={UserNavBar} options={{headerShown: false}}/>
+      <Stack.Screen name="UserNotification" component={UserNotification}/>
     </Stack.Navigator>
   );
 };
