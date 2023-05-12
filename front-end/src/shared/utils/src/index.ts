64dcd90d4fe1a976a3691ff65dc2d97a5bcec5bb
Nguyen Mai Thy <nguyenmaithy@Nguyens-MacBook-Pro.local>
2023-05-12 14:33:55 +0700

    LVTN-262: User can see tracking summary screen

--- front-end/src/shared/utils/src/index.ts
+++ front-end/src/shared/utils/src/index.ts
@@ -4,3 +4,4 @@ export * from './screen-size/screen-size';
 export * from './button-types';
 export * from './screen-size/custom-size';
 export * from './inquiry-status-type'
+export * from './tracking-place-status-type'
